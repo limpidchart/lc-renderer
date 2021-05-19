078c57a84a4b72a6e698c435c6bac937c2c905b8
Andrei Ozerov <andrei.ozerov92@gmail.com>
2021-05-19 21:14:41 +0300

    Add RendererError type
    Add enum that represents all application errors.

---
+++ src/error.rs
@@ -0,0 +1,24 @@
+/// Renderer error return type.
+#[derive(Debug)]
+pub enum RendererError {
+    /// RenderError contains chart rendering error from rendering lib.
+    RenderError(lc_render::Error),
+}
+
+impl std::fmt::Display for RendererError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &*self {
+            RendererError::RenderError(e) => {
+                format!("unable to render chart: {}", e.to_string()).fmt(f)
+            }
+        }
+    }
+}
+
+impl std::error::Error for RendererError {}
+
+impl std::convert::From<lc_render::Error> for RendererError {
+    fn from(e: lc_render::Error) -> Self {
+        RendererError::RenderError(e)
+    }
+}
