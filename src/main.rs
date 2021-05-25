5c3f34398f89cabd2bb73cf92c15bd2661245e40
Andrei Ozerov <andrei.ozerov92@gmail.com>
2021-05-25 23:05:39 +0300

    Implement ChartRenderer service
    Add `RendererServer` that implements lc-proto `ChartRenderer`.
    
    Rewrite `main` function to configure and start `RendererServer`.
    
    Cleanup `RendererError::RenderError` message.
    
    Implement JSON structured logging with `slog_json`.
    
    Update `lc-render` to `v0.2.3`.
    
    Add `tokio` dependency as the async runtime.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,63 @@
+#[macro_use]
+extern crate slog;
+extern crate slog_async;
+extern crate slog_json;
+
+use crate::proto::render::chart_renderer_server::ChartRendererServer;
+use crate::renderer::RendererServer;
+use slog::{Drain, FnValue, PushFnValue, Record};
+use std::net::SocketAddr;
+use tonic::transport::Server;
+
 mod bar;
 mod color;
 mod error;
 mod point;
 mod proto;
+mod renderer;
 mod scale;
 mod value;
 mod view;
 
-fn main() {
-    println!("Hello, world!");
+const ENV_LC_RENDERER_ADDR: &str = "LC_RENDERER_ADDR";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Prepare logger.
+    let drain = slog_json::Json::new(std::io::stdout())
+        .set_pretty(false)
+        .add_key_value(o!(
+        "ts" => PushFnValue(move |_ : &Record, ser| {
+            ser.emit(chrono::Utc::now().to_rfc3339())
+        }),
+        "level" => FnValue(move |rinfo : &Record| {
+            rinfo.level().as_short_str()
+        }),
+        "msg" => PushFnValue(move |record : &Record, ser| {
+            ser.emit(record.msg())
+        }),
+        ))
+        .build()
+        .fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    let log = slog::Logger::root(drain, o!("version" => env!("CARGO_PKG_VERSION")));
+
+    // Configure server address from env.
+    let addr = std::env::var(ENV_LC_RENDERER_ADDR).expect(&*format!(
+        "unable to read {} env variable",
+        ENV_LC_RENDERER_ADDR
+    ));
+    let socket_addr: SocketAddr = addr
+        .parse()
+        .expect(&*format!("unable to use {} as socket address", addr));
+
+    // Start GRPC server.
+    info!(log, "Server is started"; "addr" => addr);
+    let renderer_server = RendererServer::new(log);
+    Server::builder()
+        .add_service(ChartRendererServer::new(renderer_server))
+        .serve(socket_addr)
+        .await?;
+
+    Ok(())
 }
